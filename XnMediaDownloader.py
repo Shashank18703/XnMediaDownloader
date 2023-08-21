e64a5deaabfd48c29cfff8f5312e36de516fde3e
Shashank Singh Shekhawat <94420589+Shashank18703@users.noreply.github.com>
2023-08-21 08:05:49 +0530

    Add files via upload

---
+++ XnMediaDownloader.py
@@ -0,0 +1,48 @@
+import streamlit as st
+from pytube import YouTube
+import instaloader
+import os
+
+def youtube_video_download(url, r, path):
+    yt = YouTube(url)
+    with st.spinner("Downloading.....") :
+        filter_streams = yt.streams.filter(res=r, file_extension='mp4')
+        if filter_streams:
+            video = filter_streams.first()
+            video.download(path)
+            st.success(f"Youtube video is successfully downloaded and stored in {path}")
+        else:
+            st.error(f"Youtube video of {r} resolution is not available")
+
+def instagram_video_download(url, path):
+    insta_loader = instaloader.Instaloader()
+    post = instaloader.Post.from_shortcode(insta_loader.context, url.split("/")[-2])
+    with st.spinner("Downloading......") :
+        insta_loader.download_post(post, target=path)
+        st.success("Instagram video downloaded successfully!")
+
+def main():
+    st.title("XnMedia Downloader")
+    media_choice = st.radio("Choose Media", ["YouTube", "Instagram"])
+
+    if media_choice == "YouTube":
+        url = st.text_input("Enter the video url")
+        resolution = st.selectbox("Choose Resolution", ["144p", "240p", "360p", "480p", "720p", "1080p"])
+        path = st.text_input("Enter the output path for downloaded videos", key="youtube_path")
+        if not os.path.exists(path) :
+            os.makedirs(path)
+        
+        if st.button("Download YouTube Video"):
+            youtube_video_download(url, resolution, path)
+
+    elif media_choice == "Instagram":
+        url = st.text_input("Enter the Instagram reel's URL")
+        path = st.text_input("Enter the output path for downloaded videos", key="instagram_path")
+        if not os.path.exists(path) :
+            os.makedirs(path)
+        
+        if st.button("Download Instagram Video"):
+            instagram_video_download(url, path)
+
+if __name__ == "__main__":
+    main()
